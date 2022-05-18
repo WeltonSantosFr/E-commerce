cf8d6a76edc56435d207187c1043aede28d63c39
=Welton Santos <weltonsantosfr@gmail.com>
2022-05-18 12:06:18 -0300

    finalizar função da barra de pesquisa

--- js/index.js
+++ js/index.js
@@ -375,3 +375,54 @@ removeCard.forEach((element) => element.addEventListener("click", (e) => {
     removeTotalValue(e)
 
 }))
+
+const searchButton = document.querySelector(".search")
+
+searchButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    const ul = document.querySelector(".products-list")
+    ul.innerHTML = ""
+    let texto = e.target.parentNode.children[0].value
+    searchReader(data, texto)
+
+})
+
+function searchReader(data, texto) {
+    const ul = document.querySelector(".products-list")
+
+    for (let i = 0; i < data.length; i++) {
+        let nomeFormatado = data[i].nameItem.toLowerCase()
+        let textoFormatado = texto.toLowerCase()
+
+        if (nomeFormatado.includes(textoFormatado)) {
+            const newItem = document.createElement("li")
+            newItem.className = "card"
+            const itemImg = document.createElement("img")
+            itemImg.src = data[i].img
+            const itemDiv = document.createElement("div")
+            itemDiv.className = "categoria"
+            const itemCategory = document.createElement("p")
+            itemCategory.innerText = data[i].tag
+            const itemName = document.createElement("h3")
+            itemName.innerText = data[i].nameItem
+            const itemDesc = document.createElement("p")
+            itemDesc.className = "desc"
+            itemDesc.innerText = data[i].description
+            const itemPrice = document.createElement("p")
+            itemPrice.className = "preco"
+            itemPrice.innerText = `R$ ${data[i].value}`
+            const addCart = document.createElement("a")
+            addCart.id = "add-cart"
+            addCart.innerText = data[i].addCart
+            itemDiv.appendChild(itemCategory)
+            newItem.append(itemImg, itemDiv, itemName, itemDesc, itemPrice, addCart)
+            ul.appendChild(newItem)
+        }
+    }
+    if (ul.childNodes.length === 0) {
+        const message = document.createElement("p")
+        message.className = "message"
+        message.innerText = "Nenhum produto encontrado"
+        ul.appendChild(message)
+    }
+}
